cd8a36622f62a570a44182c38219bb725b940c7e
Eloi Campagne <eloi.campagne@gmail.com>
2025-06-20 17:48:16 +0200

    feat: locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,17 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Bordeaux, France",
+    44.841225,
+    -0.5800364
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Marseille, France",
+    43.2961743,
+    5.3699525
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Vilnius, Lithuania",
+    54.6870458,
+    25.2829111
   ]
 ];
